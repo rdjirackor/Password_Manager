ec205d28929322723cbfdee6e4169dc04f2a91c9
Richard <tessbry814@gmail.com>
2023-05-04 12:49:00 +0000

    I am about to fix the messup, well i say I... I mean, well you know..

--- create_account.py
+++ create_account.py
@@ -1,26 +1,24 @@
 def create_user():
-    user_name=str(input("What is your Name? :",))
-    user_email=str(input("What is your email address?, this will \n be used in case you forget your password :",))
-    user_password=str(input("Create a master password, preferable a passphrase :",))
-    return {"User":user_name, "Email":user_email,"Central_pass":user_password}
-#Te variabl below is assigned a weird meaningless name, but it means im trying to access the password the user made before, so well keep up
-pletti=create_user()
-initially_given_password=pletti["Central_pass"]
+    user_name = input("What is your Name? :")
+    user_email = input("What is your email address?, this will\nbe used in case you forget your password :")
+    user_password = input("Create a master password, preferable a passphrase :")
+    return {"User": user_name, "Email": user_email, "Central_pass": user_password}
 
 
 def confirm_password(password):
-    confirmed_one=str(input("Re-Enter the password, you forgetful geezer :",))
-    #if confirmed_one==password:
-    #    print("Well, look at you being all alert!")
-     #   return confirmed_one
-    #else:
-     #   print("The two password dont match, try again")
-     #  create_user()
-    while confirmed_one!=password:
-       print("The two password dont match, so we'll start again")
-       create_user()
-       if confirmed_one==password:
-           break
-       return confirmed_one
-final_password=confirm_password(pletti["Central_pass"])
-print(f"Your accepted password is {final_password}")
+    confirmed_one = input("Re-Enter the password, you forgetful geezer :")
+    while confirmed_one != password:
+        print("The two passwords don't match, so we'll start again")
+        user = create_user()
+        password = user["Central_pass"]
+        confirmed_one = input("Re-Enter the password, you forgetful geezer :")
+        if confirmed_one == password:
+           return confirmed_one
+            
+        
+    #print(f"Your accepted password is {confirmed_one}")
+
+
+user = create_user()
+initially_given_password = user["Central_pass"]
+confirm_password(initially_given_password)
--- user_auth.py
+++ user_auth.py
@@ -1,7 +1,20 @@
 import sqlite3 as sql
-conne=sql.connect("user_details.db")
+from create_account import create_user, confirm_password
 
+user = create_user()
+initially_given_password = user["Central_pass"]
+confirmed_password = confirm_password(initially_given_password)
 
+conne = sql.connect("user_details.db")
 
-cursor=conne.cursor()
+cursor = conne.cursor()
 cursor.execute("CREATE TABLE IF NOT EXISTS ROOT (Name TEXT, PASSWORD TEXT,EMAIL TEXT)")
+cursor.execute("INSERT INTO ROOT VALUES (?, ?, ?)", (user["User"], user["Email"], confirmed_password))
+
+cursor.execute("SELECT * FROM ROOT")
+rows = cursor.fetchall()
+for row in rows:
+    print(row)
+
+conne.commit()
+conne.close()
